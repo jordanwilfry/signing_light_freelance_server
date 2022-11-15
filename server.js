a16892dff76e38f4cb8214ec431fc9e3b487a705
Mbah  naoussi jordan wilfry <88889933+jordanwilfry@users.noreply.github.com>
2022-11-15 21:01:49 +0100

    lates changes

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const socketio = require("socket.io")
+const http = require("http")
+
+const server = http.createServer(app)
+const io = socketio(server, {
+    cors: {
+        origin: process.env.CLIENT_URL,
+    }
+})
 
 const config = require('./config');
 
@@ -12,7 +21,6 @@ const morgan = require('morgan')
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const path = require('path')
-
 
 require('dotenv').config()
 
@@ -52,6 +60,61 @@ app.post('/upload' , upload.single("file"), (req, res)=>{
     }
 })
 
+// working with socket
+
+let users = []
+
+const AddUser = (userId, socketId) => {
+    console.log(userId);
+    console.log(socketId);
+    !users.some((user)=> user.userId === userId) &&
+        users.push({ userId, socketId })
+}
+
+const RemoveUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId)    
+}
+
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+  
+  
+io.on("connection", (socket) => {
+//when connect
+
+    console.log("A user connected")
+    io.emit("welcome", 'hello this my socket server!')
+    console.log(users);
+
+    //taking the user_Id front the react application
+    socket.on("addUserConect", (userId) => {
+        AddUser(userId, socket.id)
+        io.emit("getUsers", users)
+    })
+
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        console.log(receiverId)
+        console.log(user)
+        io.to(user?.socketId).emit("getMessage", {
+        senderId,
+        text,
+        });
+    });
+
+
+//when disconnect 
+    socket.on('disconnect', ()=>{
+        console.log('a user have is disconnected')
+        RemoveUser(socket.id)
+        io.emit("getUsers", users)
+    })
+})
+
+// end socket io
+
 app.get('/' , (req,res)=>{
     res.status(200).json('Hello')
 })
@@ -65,7 +128,7 @@ app.use('/message', MessageRoute)
 app.use('/search', SearchRoute)
 
 
-app.listen(config.port, (err)=>{
+server.listen(config.port, (err)=>{
     if(!err){
         console.log(`Server succesfully started  on port ${config.port}`);
     }
